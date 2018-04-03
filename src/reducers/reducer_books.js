403fb049531eadc384b39796fe82414273ed9356
Patryk Laba <laba.patryk@gmail.com>
2018-04-03 18:56:02 +0200

    My first container

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'JavaScript: The good'},
+        {title: 'JavaScript: The bad'},
+        {title: 'JavaScript: The ugly'},
+        {title: 'JavaScript: The best'}
+    ];
+}
